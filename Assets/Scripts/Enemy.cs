8432168f6edd71441164e60aec6b09c3ee39111c
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-02-23 21:50:33 -0500

    enemy moves towards waypoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
     void Start()
     {
-        
+        target = Waypoints.wayPoints[0];
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.4)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wayPointIndex >= Waypoints.wayPoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+
+        wayPointIndex++;
+        target = Waypoints.wayPoints[wayPointIndex];
+
     }
 }
