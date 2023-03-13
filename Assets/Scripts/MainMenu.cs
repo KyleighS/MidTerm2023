e17134f6c705ff690bc638770b0e8f9c37a623fd
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-03-13 12:15:52 -0400

    Got wall added how to menu, fixed scene loader

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public void LoadLeve(string sceneName)
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(sceneName);
     }
 
     public void Quit()
